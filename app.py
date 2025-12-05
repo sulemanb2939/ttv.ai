08d766a1359aea26bb8ec26d423e97d2ccd61248
Muhammad Suleman <sulemanb2939@gmail.com>
2025-12-06 04:17:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -251,5 +251,11 @@ def download(job_id):
 # ------------------------------------------
 # RUN SERVER
 # ------------------------------------------
+# ------------------------------------------
+# RUN SERVER
+# ------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
